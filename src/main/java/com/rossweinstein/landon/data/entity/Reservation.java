4b68c0ca5a684cd3dedd0b3b7b9296511923fae9
Ross Weinstein <ross.a.weinstein@gmail.com>
2017-08-09 17:18:55 -0500

    added Reservation class

---
+++ src/main/java/com/rossweinstein/landon/data/entity/Reservation.java
@@ -0,0 +1,59 @@
+package com.rossweinstein.landon.data.entity;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name="RESERVATION")
+public class Reservation {
+
+    @Id
+    @Column(name="RESERVATION_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "ROOM_ID")
+    private long roomID;
+
+    @Column(name = "GUEST_ID")
+    private long guestID;
+
+    @Column(name = "RES_DATE")
+    private Date date;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getRoomID() {
+        return roomID;
+    }
+
+    public void setRoomID(long roomID) {
+        this.roomID = roomID;
+    }
+
+    public long getGuestID() {
+        return guestID;
+    }
+
+    public void setGuestID(long guestID) {
+        this.guestID = guestID;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
+
+
